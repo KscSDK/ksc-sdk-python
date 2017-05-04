6611ff3a89d9ce75d03d4dafefa25b34ed2a6bf9
smartwang <407718756@qq.com>
2017-05-04 14:15:25 +0800

    Update setup.py
    fix unicodedecodeerror on windows

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     name='ksc-sdk-python',
     version=kscore.__version__,
     description='Low-level, data-driven core of ksc.',
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst', encoding='utf-8').read(),
     author=AUTHOR,
     url='https://github.com/liuyichen/kscore',
     author_email=AUTHOR_EMAIL,
