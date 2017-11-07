aa328ef0e9f50cbad795cc22b465a26ef4941707
liuyc <liuyc.mail@gmail.com>
2017-11-07 13:27:34 +0800

    Update kec.py
    修正云主机的用例。

--- examples/kec.py
+++ examples/kec.py
@@ -5,12 +5,22 @@ from kscore.session import get_session
 if __name__ == "__main__":
     s = get_session()
 
-    client = s.create_client("kec", "cn-beijing-6", use_ssl=True)
+    client = s.create_client("kec", "cn-beijing-6", use_ssl=False)
 
-    print client.describe_instances()
+    # https://docs.ksyun.com/read/latest/52/_book/oaDescribeInstances.html
+    client.describe_instances()
 
-#    client.create_user(UserName="test22", RealName=u"刘一辰")
+    # https://docs.ksyun.com/read/latest/52/_book/oaRunInstances.html
+    client.run_instances(
+        MaxCount=50, MinCount=20, ImageId="3f3bddcf-4982-4ab4-a63d-795e8d74e9d5",
+        SubnetId="f1bd236b-7fd3-44d3-aef9-2d673a65466e", InstancePassword="Ksyun2017",
+        SecurityGroupId="2f43a9e4-1a3c-448e-b661-efa6d04b82fc", DataDiskGb=50, ChargeType="Monthly",
+        InstanceType="C1.1A", PurchaseTime=1, InstanceName="test", InstanceNameSuffix="1")
+    
+    # https://docs.ksyun.com/read/latest/52/_book/oaTerminateInstances.html
+    instances = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
 
-#    client.update_user(UserName="test22",)
+    instances = dict(("InstanceId.{}".format(index), instance) for index, instance in enumerate(instances, 1))
 
-#    client.delete_user(UserName="test22")
+    client.terminate_instances(**instances)
+
