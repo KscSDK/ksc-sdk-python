4702c8abde8bef0d56052992de5454fd81508ba8
Xu Yaming <xuyaming@kingsoft.com>
2018-06-08 11:35:09 +0800

    增加注释

--- examples/DedicatedVmCreate.py
+++ examples/DedicatedVmCreate.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python
-
-# -*- encoding:utf-8 -*-
+#coding=utf-8
 
 from kscore.session import get_session
 import sys
 
-#inputfile format（csv）
-#InstanceName,Vcpus,Memory,DataDiskSize,InstancePassword,PrivateIpAddress,SubnetId,SecurityGroupId,DedicatedHostId,ImageId
+#使用方法
+'''
+python python DedicatedVmCreate.py inputfile.csv
+'''
+
+#输入文件格式（csv文件）示例
+'''
+InstanceName,Vcpus,Memory,DataDiskSize,InstancePassword,PrivateIpAddress,SubnetId,SecurityGroupId,DedicatedHostId,ImageId
+Vm-1,4,8,100,123@123,10.0.0.1,9c29fe3e-6a16-41d0-85e3-94c52ffb1038,2d0ab207-6fa5-451c-b0f5-34d7244fd424,ac5a9f97-ab38-40ce-b284-df14282e0916,3dc0a83e-2dbf-4fd4-99c4-e620fcf4d849
+'''
+#属性解释
+'''
+InstanceName 主机名称
+Vcpus cpu数量
+Memory 内存大小
+DataDiskSize 数据盘大小
+InstancePassword 主机密码
+PrivateIpAddress 内网IP
+SubnetId 子网ID
+SecurityGroupId 安全组ID
+DedicatedHostId 专属宿主机ID
+ImageId 镜像ID
+'''
 
 ak ='ak'
 sk = 'sk'
