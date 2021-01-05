8fe00425f6d834b2271c1c0049b6a1a6d039b77d
wangyanan2 <wangyanan2@kingsoft.com>
2021-01-05 18:50:10 +0800

    补充自测case

--- examples/kec.py
+++ examples/kec.py
@@ -1,10 +1,10 @@
 # -*- encoding:utf-8 -*-
-
+from kscore.exceptions import ClientError
 from kscore.session import get_session
+from pprint import pprint
 
 if __name__ == "__main__":
     s = get_session()
-
     client = s.create_client("kec", "cn-beijing-6", use_ssl=False)
 
     # https://docs.ksyun.com/read/latest/52/_book/oaDescribeInstances.html
@@ -12,11 +12,11 @@ if __name__ == "__main__":
 
     # https://docs.ksyun.com/read/latest/52/_book/oaRunInstances.html
     client.run_instances(
-        MaxCount=50, MinCount=20, ImageId="3f3bddcf-4982-4ab4-a63d-795e8d74e9d5",
-        SubnetId="f1bd236b-7fd3-44d3-aef9-2d673a65466e", InstancePassword="Ksyun2017",
-        SecurityGroupId="2f43a9e4-1a3c-448e-b661-efa6d04b82fc", DataDiskGb=50, ChargeType="Monthly",
-        InstanceType="C1.1A", PurchaseTime=1, InstanceName="test", InstanceNameSuffix="1")
-    
+         MaxCount=50, MinCount=20, ImageId="3f3bddcf-4982-4ab4-a63d-795e8d74e9d5",
+         SubnetId="f1bd236b-7fd3-44d3-aef9-2d673a65466e", InstancePassword="Ksyun2017",
+         SecurityGroupId="2f43a9e4-1a3c-448e-b661-efa6d04b82fc", DataDiskGb=50, ChargeType="Monthly",
+         InstanceType="C1.1A", PurchaseTime=1, InstanceName="test", InstanceNameSuffix="1")
+
     # https://docs.ksyun.com/read/latest/52/_book/oaTerminateInstances.html
     instances = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
 
@@ -25,117 +25,142 @@ if __name__ == "__main__":
     client.terminate_instances(**instances)
 
     #用户查询竞价实例历史价格列表
-    client.describe_spot_price_history(InstanceType="E1.1A", AvailabilityZone="cn-shanghai-3a")
+    # pprint("-------------------------------用户查询竞价实例历史价格列表---------------------------------")
+    # _spot_price = client.describe_spot_price_history(InstanceType="E1.1A", AvailabilityZone="cn-shanghai-3a")
+    # pprint(_spot_price)
 
     #查看主机价格
-    client.describe_price(InstanceType="I1.8B", ImageId="e00e5b9a-0914-4611-8730-3768bf08e035", ChargeType="Daily", SriovNetSupport=True)
+    # pprint("-------------------------------查看主机价格---------------------------------")
+    # _instance_price = client.describe_price(InstanceType="N3.1A", ImageId="IMG-5a7cb0e5-d297-4259-9944-38b3c053a7dc", ChargeType="Daily", SriovNetSupport=True)
+    # pprint(_instance_price)
 
     #创建实例启动模板
-    client.create_model(ImageId="", SubnetId="", ChargeType="Daily", ModelName="", SecurityGroupId="", PurchaseTime=0)
+    # pprint("-------------------------------创建实例启动模板---------------------------------")
+    # _instance_model = client.create_model(ImageId="IMG-5a7cb0e5-d297-4259-9944-38b3c053a7dc",
+    #                                       SubnetId="afa859fc-65ea-410d-99f2-d686cf9da1d9",
+    #                                       ChargeType="Daily", ModelName="wyn_test_model",
+    #                                       SecurityGroupId="18523073-c6c0-44ba-9858-1c0f92cb453d", PurchaseTime=0,
+    #                                       InstanceType="N3.1A")
+    # pprint(_instance_model)
 
     #删除实例启动模板
-    models = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    models = dict(("ModelId.{}".format(index), model) for index, model in enumerate(models, 1))
-    client.terminate_models(**models)
+    # pprint("-------------------------------删除实例启动模板---------------------------------")
+    # models = ["0565ffc5-55e3-44e9-b242-5ed3de725ee0"]
+    # models = dict(("ModelId.{}".format(index), model) for index, model in enumerate(models, 1))
+    # _del_model = client.terminate_models(**models)
+    # pprint(_del_model)
 
     #查看实例启动模板
-    client.describe_models()
+    # pprint("-------------------------------查看实例启动模板---------------------------------")
+    # _des_models = client.describe_models()
+    # pprint(_des_models)
 
     #云主机迁入容灾组
-    instances = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    instances = dict(("InstanceId.{}".format(index), instance) for index, instance in enumerate(instances, 1))
-    client.add_vm_into_data_guard(DataGuardId="", **instances)
+    # pprint("-------------------------------云主机迁入容灾组-------------------------------")
+    # instances = ["5e78a001-e472-44f5-9aa4-c822a8121319"]
+    # instances = dict(("InstanceId.{}".format(index), instance) for index, instance in enumerate(instances, 1))
+    # add_vm_into_data_guard = client.add_vm_into_data_guard(DataGuardId="ec148c88-5958-49dc-97a2-76fc21c4552a", **instances)
+    # pprint(add_vm_into_data_guard)
 
     #修改容灾分组名称
-    client.modify_data_guard_groups(DataGuardId="", DataGuardName="")
+    # pprint("-------------------------------修改容灾分组名称--------------------------------")
+    # mod_data_guard_group_name = client.modify_data_guard_groups(DataGuardId="ec148c88-5958-49dc-97a2-76fc21c4552a", DataGuardName="wyn_test")
+    # pprint(mod_data_guard_group_name)
 
     #查询用户某区域的容灾分组容量
-    client.describe_data_guard_capacity()
+    # pprint("-------------------------------查询用户某区域的容灾分组容量--------------------------------")
+    # des_data_guard_capacity = client.describe_data_guard_capacity()
+    # pprint(des_data_guard_capacity)
 
     #创建容灾分组
-    client.create_data_guard_group(DataGuardName="my_dataguard_test")
+    # pprint("-------------------------------创建容灾分组---------------------------------")
+    # cre_data_guard_group = client.create_data_guard_group(DataGuardName="my_dataguard_test")
+    # pprint(cre_data_guard_group)
 
     #删除容灾分组
-    data_guards = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    data_guards = dict(("DataGuardId.{}".format(index), data_guard) for index, data_guard in enumerate(data_guards, 1))
-    client.delete_data_guard_groups(**data_guards)
+    # pprint("-------------------------------删除容灾分组---------------------------------")
+    # data_guards = ["ec148c88-5958-49dc-97a2-76fc21c4552a"]
+    # data_guards = dict(("DataGuardId.{}".format(index), data_guard) for index, data_guard in enumerate(data_guards, 1))
+    # del_data_guard_groups = client.delete_data_guard_groups(**data_guards)
+    # pprint(del_data_guard_groups)
 
     #查询容灾组信息
-    client.describe_data_guard_group()
+    # pprint("-------------------------------查询容灾组信息---------------------------------")
+    # des_data_guard_group = client.describe_data_guard_group()
+    # pprint(des_data_guard_group)
 
     #云主机从容灾组中移除
-    instances = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    instances = dict(("InstanceId.{}".format(index), instance) for index, instance in enumerate(instances, 1))
-    client.remove_vm_from_data_guard(DataGuardId="", **instances)
+    # pprint("-------------------------------云主机从容灾组中移除---------------------------------")
+    # instances = ["5e78a001-e472-44f5-9aa4-c822a8121319"]
+    # instances = dict(("InstanceId.{}".format(index), instance) for index, instance in enumerate(instances, 1))
+    # remove_vm_from_data_guard = client.remove_vm_from_data_guard(DataGuardId="ec148c88-5958-49dc-97a2-76fc21c4552a", **instances)
+    # pprint(remove_vm_from_data_guard)
 
-    #镜像导入
-    client.import_image(ImageName="", Architecture="", Platform="", ImageUrl="", ImageFormat="")
+    # #镜像导入
+    # client.import_image(ImageName="", Architecture="", Platform="", ImageUrl="", ImageFormat="")
 
     #镜像复制
-    images = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    images = dict(("ImageId.{}".format(index), image) for index, image in enumerate(images, 1))
-    destination_regions = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    destination_regions = dict(("DestinationRegion.{}".format(index), destination_region) for index, destination_region in enumerate(destination_regions, 1))
-    client.copy_image(**images, **destination_regions)
+    # pprint("-------------------------------镜像复制---------------------------------")
+    # param = {
+    #     "ImageId.1": "432eb1f8-72f3-4320-a0f7-572b33b32431",
+    #     "DestinationRegion.1": "cn-shanghai-3"
+    # }
+    # copy_image = client.copy_image(**param)
+    # pprint(copy_image)
 
     #查看镜像分享信息
-    client.describe_image_share_permission(ImageId="", RequestId="", SharePermissionSet="")
+    # pprint("-------------------------------查看镜像分享信息---------------------------------")
+    # describe_image_share_permission = client.describe_image_share_permission(ImageId="e5b7d077-021c-4fb2-9d0e-fa4700f99b58")
+    # pprint(describe_image_share_permission)
 
     #修改镜像分享信息
-    accounts = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    accounts = dict(("AccountId.{}".format(index), account) for index, account in enumerate(accounts, 1))
-    client.modify_image_share_permission(ImageId="", Permission="", **accounts)
-
-    #查询云市场镜像信息
-    client.query_image_list(Region="")
+    # pprint("-------------------------------修改镜像分享信息---------------------------------")
+    # accounts = ["73403544"]
+    # accounts = dict(("AccountId.{}".format(index), account) for index, account in enumerate(accounts, 1))
+    # modify_image_share_permission = client.modify_image_share_permission(ImageId="e5b7d077-021c-4fb2-9d0e-fa4700f99b58", Permission="share", **accounts)
+    # pprint(modify_image_share_permission)
 
     #镜像预热
-    client.enable_image_caching(ImageId="")
+    # pprint("-------------------------------镜像预热---------------------------------")
+    # enable_image_caching = client.enable_image_caching(ImageId="432eb1f8-72f3-4320-a0f7-572b33b32431")
+    # pprint(enable_image_caching)
 
     #取消镜像预热
-    client.disable_image_caching(ImageId="")
+    # pprint("-------------------------------取消镜像预热---------------------------------")
+    # disable_image_caching = client.disable_image_caching(ImageId="432eb1f8-72f3-4320-a0f7-572b33b32431")
+    # pprint(disable_image_caching)
 
     #创建本地盘快照
-    client.create_local_volume_snapshot(LocalVolumeId="", LocalVolumeSnapshotName="")
+    # pprint("-------------------------------创建本地盘快照---------------------------------")
+    # create_local_volume_snapshot = client.create_local_volume_snapshot(LocalVolumeId="58ce95e4-0e8f-44e1-8134-dca14697c103-a", LocalVolumeSnapshotName="wyn_test")
+    # pprint(create_local_volume_snapshot)
 
     #回滚快照
-    client.rollback_local_volume(LocalVolumeId="", LocalVolumeSnapshotId="")
+    # pprint("-------------------------------回滚快照---------------------------------")
+    # rollback_local_volume = client.rollback_local_volume(LocalVolumeSnapshotId="6c08b3ff-34c2-4948-b81d-4c8cc9b7a084")
+    # pprint(rollback_local_volume)
 
     #删除快照
-    local_volume_snapshots = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    local_volume_snapshots = dict(("LocalVolumeSnapshotId.{}".format(index), local_volume_snapshot) for index, local_volume_snapshot in enumerate(local_volume_snapshots, 1))
-    client.delete_local_volume_snapshot(**local_volume_snapshots)
-
-    #创建弹性网卡
-    security_groups = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    security_groups = dict(("SecurityGroupId.{}".format(index), security_group) for index, security_group in enumerate(security_groups, 1))
-    client.create_network_interface(SubnetId="", **security_groups)
-
-    #删除弹性网卡
-    client.delete_network_interface(NetworkInterfaceId="")
-
-    #描述网卡信息
-    client.describe_network_interfaces()
-
-    #创建密钥
-    client.create_key()
-
-    #导入密钥
-    client.import_key(KeyName="", PublicKey="")
-
-    #删除密钥
-    client.delete_key(KeyId="")
+    # pprint("-------------------------------删除快照---------------------------------")
+    # local_volume_snapshots = ["6c08b3ff-34c2-4948-b81d-4c8cc9b7a084"]
+    # local_volume_snapshots = dict(("LocalVolumeSnapshotId.{}".format(index), local_volume_snapshot) for index, local_volume_snapshot in enumerate(local_volume_snapshots, 1))
+    # delete_local_volume_snapshot = client.delete_local_volume_snapshot(**local_volume_snapshots)
+    # pprint(delete_local_volume_snapshot)
 
     #主机绑定密钥
-    instances = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    instances = dict(("InstanceId.{}".format(index), instance) for index, instance in enumerate(instances, 1))
-    keys = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    keys = dict(("KeyId.{}".format(index), key) for index, key in enumerate(keys, 1))
-    client.attach_key(**instances, **keys)
+    # pprint("-------------------------------主机绑定密钥---------------------------------")
+    # param = {
+    #     "InstanceId.1": "5e78a001-e472-44f5-9aa4-c822a8121319",
+    #     "KeyId.1": "c079a41f-beb4-4b73-a68c-69f25b9c8819"
+    # }
+    # attach_key = client.attach_key(**param)
+    # pprint(attach_key)
 
     #主机解绑密钥
-    instances = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    instances = dict(("InstanceId.{}".format(index), instance) for index, instance in enumerate(instances, 1))
-    keys = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
-    keys = dict(("KeyId.{}".format(index), key) for index, key in enumerate(keys, 1))
-    client.detach_key(**instances, **keys)
+    # pprint("-------------------------------主机解绑密钥---------------------------------")
+    # param = {
+    #     "InstanceId.1": "5e78a001-e472-44f5-9aa4-c822a8121319",
+    #     "KeyId.1": "c079a41f-beb4-4b73-a68c-69f25b9c8819"
+    # }
+    # detach_key = client.detach_key(**param)
+    # pprint(detach_key)
