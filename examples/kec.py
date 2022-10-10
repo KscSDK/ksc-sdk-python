3300f38d0a7b8e2e8aa44b9f149aa88a525f7760
x6doooo <x6doooo@gmail.com>
2022-10-10 11:35:59 +0800

    update: 更新api文档url

--- examples/kec.py
+++ examples/kec.py
@@ -7,17 +7,17 @@ if __name__ == "__main__":
     s = get_session()
     client = s.create_client("kec", "cn-beijing-6", use_ssl=False)
 
-    # https://docs.ksyun.com/read/latest/52/_book/oaDescribeInstances.html
+    # https://docs.ksyun.com/documents/816?type=3
     client.describe_instances()
 
-    # https://docs.ksyun.com/read/latest/52/_book/oaRunInstances.html
+    # https://docs.ksyun.com/documents/806?type=3
     client.run_instances(
         MaxCount=50, MinCount=20, ImageId="3f3bddcf-4982-4ab4-a63d-795e8d74e9d5",
         SubnetId="f1bd236b-7fd3-44d3-aef9-2d673a65466e", InstancePassword="Ksyun2017",
         SecurityGroupId="2f43a9e4-1a3c-448e-b661-efa6d04b82fc", DataDiskGb=50, ChargeType="Monthly",
         InstanceType="C1.1A", PurchaseTime=1, InstanceName="test", InstanceNameSuffix="1")
 
-    # https://docs.ksyun.com/read/latest/52/_book/oaTerminateInstances.html
+    # https://docs.ksyun.com/documents/809?type=3
     instances = ["2f43a9e4-1a3c-448e-b661-efa6d04b82fc", "2f43a9e4-1a3c-448e-b661-efa6d04b82fc"]
 
     instances = dict(("InstanceId.{}".format(index), instance) for index, instance in enumerate(instances, 1))
