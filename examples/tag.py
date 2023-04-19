31d5db062d28d5720eba1d3d654e9cf67f4920c8
yangdongxu <yangdongxu@kingsoft.com>
2023-04-19 11:33:33 +0800

    update: 增加tag的示例

--- examples/tag.py
+++ examples/tag.py
@@ -9,3 +9,10 @@ if __name__ == "__main__":
     client = s.create_client("tag", region_name="cn-beijing-6", use_ssl=True)
 
     print client.describe_tags()
+
+    # https://docs.ksyun.com/documents/1327
+    # 根据资源id查询tag
+    print client.describe_tags(**{
+        "Filter.1.Name": "resource-id",
+        "Filter.1.Value.1": "fc175f56-d29d-497f-9f57-xxxxxxxxxxxx",
+    })
